4d4e2311dda043efcce02f00e8bf655055887bf8
Adrien Vinches <viinchou@gmail.com>
2015-11-17 19:34:37 -0800

    fix search view more

--- pinehust-ui/app/components/search-list.js
+++ pinehust-ui/app/components/search-list.js
@@ -6,6 +6,47 @@ export default Ember.Component.extend({
   artistsVisible: false,
   albumsVisible: false,
 
+  getClass: function (resultLength) {
+    if (resultLength < 7) {
+      return 'compact';
+    } else if (resultLength < 13) {
+      return 'full1';
+    } else if (resultLength < 19) {
+      return 'full2';
+    } else if (resultLength < 25) {
+      return 'full3';
+    } else if (resultLength < 31) {
+      return 'full4';
+    } else if (resultLength < 37) {
+      return 'full5';
+    } else if (resultLength < 43) {
+      return 'full6';
+    } else if (resultLength < 49) {
+      return 'full7';
+    } else {
+      return 'full8';
+    }
+  },
+
+  artistClass: function () {
+    if (!this.get('artistsVisible')) {
+      return 'compact';
+    } else {
+      var resultLength = this.get('search').get('listSearchArtistsResults').get('length');
+      return this.getClass(resultLength);
+    }
+  }.property('artistsVisible,search.listSearchArtistsResults'),
+
+  albumClass: function () {
+    if (!this.get('albumsVisible')) {
+      return 'compact';
+    } else {
+      var resultLength = this.get('search').get('listSearchAlbumsResults').get('length');
+      return this.getClass(resultLength);
+    }
+  }.property('albumsVisible,search.listSearchAlbumsResults'),
+
+
   listSearchArtistsResultsDisplayed: function () {
     return this.get('search').get('listSearchArtistsResults');
   }.property('search.listSearchArtistsResults'),
